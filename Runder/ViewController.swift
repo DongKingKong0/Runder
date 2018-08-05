0b8dd27acedd6ce32dfbcfa2eef846c0d461133a
DongKingKong0 <dongkingkong0@gmail.com>
2018-08-05 19:57:50 +0200

    get GPS coordinates

---
+++ Runder/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Runder
+//
+//  Created by Lorenzo on 05.08.18.
+//  Copyright © 2018 New Image. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        
+        locationLabel.text = "x: \(locValue.latitude) y: \(locValue.longitude)"
+    }
+    
+}
+
